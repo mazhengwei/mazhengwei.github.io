c6a1f83a30608169d0b089bf56a5b97b60569d5a
慕言 <mazhengwei@cai-inc.com>
2023-06-08 08:04:25 +0800

    新增备案信息

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -84,7 +84,7 @@ module.exports = {
     },
     footer: { // 页脚信息
         createYear: 2022, // 博客创建年份
-        copyrightInfo: '| <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602013068" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="/img/png/备案图标.png" style="float:left;"/>浙公网安备 33010602013068号</a>', // 博客版权信息，支持a标签
+        copyrightInfo: '| <a href="https://beian.miit.gov.cn/" target="_blank">浙ICP备2023011587号-1</a> | <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602013068" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="/img/png/备案图标.png" style="float:left;"/>浙公网安备 33010602013068号</a>', // 博客版权信息，支持a标签
     },
     htmlModules,
 }
